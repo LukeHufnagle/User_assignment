43d2f4b0b06489b3e396980ef5360ee39bcb35c4
Luke Hufnagle <leh17068@gmail.com>
2021-08-31 14:48:51 -0400

    Chained Methods

--- User.py
+++ User.py
@@ -7,37 +7,32 @@ class User:
         self.balance += amount
         print(f"Depositing {amount} into account {self.name}")
         print(f"Your balance is now {self.balance}")
+        return self
     
     def makeWithdrawal(self, amount):
         self.balance -= amount
         print(f"Withdrawing {amount} from account {self.name}")
         print(f"Your balance is now {self.balance}")
+        return self
 
     def displayBalance(self,balance):
         print(balance)
+        return self
 
 
 Jake = User("Jake", 300);
 Jack = User("Jack", 200);
 John = User("John", 100);
 
-Jake.makeDeposit(100)
-Jake.makeDeposit(200)
-Jake.makeDeposit(400)
-Jake.makeWithdrawal(200)
+Jake.makeDeposit(100).makeDeposit(200).makeDeposit(400).makeWithdrawal(200)
 print(f"Jake's balance is now: " + str(Jake.balance))
 
 print("------------------------------------------------------")
 
-Jack.makeDeposit(1000)
-Jack.makeDeposit(2000)
-Jack.makeWithdrawal(500)
-Jack.makeWithdrawal(500)
+Jack.makeDeposit(1000).makeDeposit(2000).makeWithdrawal(500).makeWithdrawal(500)
 print(f"Jack's balance is now: " + str(Jack.balance))
 
 print("------------------------------------------------------")
 
-John.makeDeposit(5000)
-John.makeWithdrawal(1000)
-John.makeWithdrawal(1000)
-John.makeWithdrawal(1000)
+John.makeDeposit(5000).makeWithdrawal(1000).makeWithdrawal(1000).makeWithdrawal(1000)
+print(f"John's balance is now: " + str(John.balance))
